a2d8b2733414f82e485fbd2d45ecd9edbd4c4a7c
navid-ka <psychodelictrip@gmail.com>
2021-01-27 12:07:11 +0100

    no-swup en contact

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,8 @@
 const swup = new Swup();
-/* const loadingScreen = document.querySelector(".loading-screen");
-const fade = document.querySelector(".fade-out");
 
+/* const contact = document.getElementById('contact');
+console.log(contact)
 
-//Anims
-
-function pageTransitionIn() {
-	return gsap.to(loadingScreen, {
-		width: "100%",
-		duration: 0.5,
-	});
-}
-
-function pageTransitionOut(container) {
-	return gsap.timeline({ delay: 0.3 }).add("start").to(
-		loadingScreen,
-		{
-			width: "0%",
-			duration: 0.3,
-			ease: "power1.out",
-		},
-		"start"
-	);
-}
- */
+contact.addEventListener('onclick', ()=> {
+	location.reload();
+}) */
