1211d4b6f11920876cdd24dad9544b3c38938bb0
navid-ka <psychodelictrip@gmail.com>
2021-02-05 12:54:34 +0100

    Contact form

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,4 @@
-window.addEventListener('scroll', () => {
-	var element = document.querySelector('.wow');
-	if(element.scrollTop >= 100) {
-		console.log(element)
-		element.classList.remove('animate__animated');
-		element.removeAttribute('style')
-		new WOW().init();
-	}
-});
-
-
-
-
 new WOW().init();
-
-
-
 const swup = new Swup({
 	plugins: [
 		/* new SwupJsPlugin(options), */
@@ -25,125 +9,48 @@ const swup = new Swup({
 	],
 });
 
-
-
-/* const loadingScreen = document.querySelector(".loading-screen");
-const options = [
-	{
-		from: "(.*)",
-		to: "(.*)",
-		in: (next) => {
-            gsap.to(loadingScreen, {
-                width: "100%",
-                duration: 0.5,
-                onComplete: next,
-            });
-            gsap.registerPlugin(ScrollTrigger) 
-            const sections = gsap.utils.toArray("section").forEach((section) => {
-                gsap.from(section, {
-                    scrollTrigger: {
-                        trigger: section,
-                        start: "top 95%",
-                        end: "top 5%",
-                        toggleActions: "play none restart none",
-                        markers: true,
-                    },
-                    opacity: 0,
-                    x: -50,
-                    ease: "Power2.easeIn",
-                    onComplete: next,
-                })
-            })
-            
-		},
-		out: (next) => {
-            gsap.timeline({ delay: 0.3 }).add("start").to(
-                loadingScreen,
-                {
-                    width: "0%",
-                    duration: 0.3,
-                    ease: "power1.out",
-                    onComplete: next,
-                },
-                "start"
-            );
-		},
-	},
-];
- */
-
-//Animations
-
-/* gsap.from(".container", {
-	scrollTrigger: {
-		trigger: ".container",
-		toggleActions: "play none restart none",
-		markers: true,
-	},
-	opacity: 0,
-	delay: 1,
-	x: -50,
-	ease: "Power2.easeIn",
-}); */
-
 // contact
 
-function enviar() {
-	var messagesElement = document.getElementById("messages");
-
-	// disable send button
-	document.querySelector("#send").disabled = true;
-	document.querySelector("#send").innerText = "Enviando ...";
-	// show messages
-	document.querySelector("#messages").style.display = "block";
-
-	// XHR object
-	var xhr = new XMLHttpRequest();
-
-	xhr.onload = function (res) {
-		if (xhr.status == 200) {
-			console.log("resultado:" + xhr.status);
-			var res = JSON.parse(this.response);
-
-			document.querySelector("#messages").innerHTML = res.message;
-			//document.querySelector("#messages").innerHTML += res.msg_phpmailer;
-			console.log(xhr.responseText);
-
-			if (res.error == false) {
-				document.querySelector("#contact").style.display = "none";
-				document
-					.querySelector("#messages")
-					.classList.remove("alert-danger");
-				document
-					.querySelector("#messages")
-					.classList.add("alert-success");
-				document.querySelector("#messages").innerHTML = res.message;
-			} else {
-				document
-					.querySelector("#messages")
-					.classList.add("alert-danger");
-				document.querySelector("#send").disabled = false;
-				document.querySelector("#send").innerText =
-					"Reintentar el envío";
-			}
-		} else {
-			// log error
-			document.querySelector("#send").disabled = false;
-			document.querySelector("#messages").innerHTML =
-				"Se ha producido un error, contacte con el administrador";
-			console.log("error" + xhr.status);
-		}
-	};
-
-	// preapare request
-	var formMethod = "POST";
-	var formUrl = "mod/enviar.php";
-	var formType = true;
-
-	xhr.open(formMethod, formUrl, formType);
-
-	// prepare form data
-	var formElement = document.getElementById("contact");
-	var data = new FormData(formElement);
-	xhr.send(data);
+function inicia() {
+	var send = document.getElementById("send");
+	send.addEventListener("click", envia);
 }
+function envia() {
+	var datos = new FormData();
+	var nombre = document.getElementById("floatingInput").value;
+	var apellidos = document.getElementById("floatingApellidos").value;
+	var correo = document.getElementById("floatingEmail").value;
+	var comentario = document.getElementById("floatingTextarea").value;
+	var condiciones = document.getElementById("flexRadio").value;
+	if (
+		nom != "" &&
+		correo != "" &&
+		apellidos != "" &&
+		comentario != "" &&
+		condiciones != ""
+	) {
+		datos.append("nombre", nombre);
+		datos.append("apellidos", apellidos);
+		datos.append("correo", correo);
+		datos.append("comentario", comentario);
+		datos.append("condiciones", condiciones);
+		var url = "mod/enviar.php";
+		var solicitud = new XMLHttpRequest();
+		solicitud.addEventListener("load", mostra);
+		solicitud.open("POST", url, true);
+		solicitud.send(datos);
+	}
+}
+function mostra(event) {
+	var datos = event.target;
+	if (datos.status == 200) {
+		document.querySelector("#messages").classList.add("alert-success");
+		document.querySelector("#messages").innerHTML =
+		datos.responseText;
+	} else {
+		document.querySelector("#messages").classList.add("alert-danger");
+		document.querySelector("#messages").innerHTML =
+			"Se ha producido un error en el envio.";
+	}
+}
+window.addEventListener("load", inicia);
